c5a3eed9b07ed72391158a71229833d02c74a1c7
umit-ultimate <umitbilgin1992>
2026-06-13 18:56:54 +0300

    Initial Commit

---
+++ PaniniTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PaniniTracker
+//
+//  Created by Gizem Bilgin on 13.06.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PaniniTrackerTests/PaniniTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  PaniniTrackerTests.swift
+//  PaniniTrackerTests
+//
+//  Created by Gizem Bilgin on 13.06.2026.
+//
+
+import Testing
+@testable import PaniniTracker
+
+struct PaniniTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
